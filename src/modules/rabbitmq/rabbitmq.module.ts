09b607a2ac430206d8a8cc8dfb24828fff462a51
ocsoares <casoares016@gmail.com>
2023-04-15 20:59:18 -0300

    refactor: add RABBITMQ_URI env to rabbitmq module

--- src/modules/rabbitmq/rabbitmq.module.ts
+++ src/modules/rabbitmq/rabbitmq.module.ts
@@ -7,7 +7,7 @@ import { Global, Module } from '@nestjs/common';
         // Async to allow tests to access .env !
         RabbitMQModule.forRootAsync(RabbitMQModule, {
             useFactory: async () => ({
-                uri: `amqp://${process.env.RABBITMQ_DEFAULT_USER}:${process.env.RABBITMQ_DEFAULT_PASS}@localhost:5672`,
+                uri: process.env.RABBITMQ_URI,
             }),
         }),
     ],
