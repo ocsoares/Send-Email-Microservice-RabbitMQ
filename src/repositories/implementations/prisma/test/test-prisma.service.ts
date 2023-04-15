e400346b3948cc89ba4bf4a230089a63c07a2d91
ocsoares <casoares016@gmail.com>
2023-04-15 19:52:56 -0300

    test: add test-prisma service

---
+++ src/repositories/implementations/prisma/test/test-prisma.service.ts
@@ -0,0 +1,15 @@
+import { PrismaService } from '../prisma-client.service';
+
+// Aplicar as Migrations trocando a url do schema.prisma pelo DB de teste,
+// depois voltar ao de produção
+export class TestPrismaService extends PrismaService {
+    constructor() {
+        super({
+            datasources: {
+                db: {
+                    url: process.env.TEST_PRISMA_URL,
+                },
+            },
+        });
+    }
+}
