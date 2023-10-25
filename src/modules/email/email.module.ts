c998914943f637dd39b538edc528ff0de01d14aa
ocsoares <casoares016@gmail.com>
2023-10-24 21:42:55 -0300

    feat: add rate limiter middleware to send email route

--- src/modules/email/email.module.ts
+++ src/modules/email/email.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { PublishSendEmailService } from './use-cases/send-email/publish/publish-send-email.service';
 import { ConsumerSendEmailService } from './use-cases/send-email/consumer/consumer-send-email.service';
 import { PublishSendEmailController } from './use-cases/send-email/publish/publish-send-email.controller';
 import { NodemailerModule } from '../nodemailer/nodemailer.module';
+import { rateLimiterMiddleware } from '../auth/middlewares/rate-limiter.middleware';
 
 @Module({
     imports: [NodemailerModule],
     providers: [PublishSendEmailService, ConsumerSendEmailService],
     controllers: [PublishSendEmailController],
 })
-export class EmailModule {}
+export class EmailModule implements NestModule {
+    // I used this because NestJS Throttler Module doesn't work !!!
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(rateLimiterMiddleware).forRoutes('send');
+    }
+}
