16cd41d6ecb29d0b814fcd3751e3281c4692975d
ocsoares <casoares016@gmail.com>
2023-11-26 11:13:33 -0300

    feat: add jwt public key security

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -10,7 +10,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secretOrKey: process.env.JWT_SECRET,
+            secretOrKey: Buffer.from(process.env.JWT_PUBLIC_KEY, 'base64'),
         });
     }
 
